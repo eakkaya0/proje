b1db0aee91423a77d39e5d9d3b074f0bd4cf2d1c
eakkaya <emre.akkaya@ogr.ksbu.edu.tr>
2024-12-31 00:19:57 +0300

    ilk commit

---
+++ 21.vizefinalörnek/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21.vizefinalörnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            durum();
+            Console.ReadLine();
+        }
+
+        private static void durum()
+        {
+            double v, f,ort;
+            Console.WriteLine("vize giriniz");
+          v= Convert.ToDouble(Console.ReadLine());
+           Console.WriteLine("final giriniz");
+          f=Convert.ToDouble(Console.ReadLine());
+            ort = v * 0.4 + f * 0.6;
+            string cevap = v == 0 ? "kaldin" : ort >= 50 ? "gectin" : ort < 50 ? "kaldiniz":"" ;
+            Console.WriteLine(cevap);
+        }
+    }
+}
